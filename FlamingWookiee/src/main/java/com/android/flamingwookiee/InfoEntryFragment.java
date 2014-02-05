f134508bf96ffde83d477956450b7842429cd442
afm0005 <rememberthisone334@gmail.com>
2014-02-05 02:22:53 -0600

    info entry dialog working
    Not completely satisfied with it but I think it's functional. Basically
    just disabled button until there are no entry errors using textwatchers
    to check as text is entered.
    And I'm not sure if i just need to commit these files or not, these are
    the only two I changed.

--- FlamingWookiee/src/main/java/com/android/flamingwookiee/InfoEntryFragment.java
+++ FlamingWookiee/src/main/java/com/android/flamingwookiee/InfoEntryFragment.java
@@ -7,8 +7,11 @@ import android.content.DialogInterface;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -21,7 +24,7 @@ public class InfoEntryFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         View v = getActivity().getLayoutInflater().inflate(R.layout.fragment_info_entry, null);
         SharedPreferences mPref = getActivity().getSharedPreferences("info", Context.MODE_PRIVATE);
@@ -29,6 +32,21 @@ public class InfoEntryFragment extends DialogFragment {
         mUsernameField = (EditText)v.findViewById(R.id.username);
         mPinNumberField = (EditText)v.findViewById(R.id.pin_number);
 
+
+        builder.setTitle("Enter Info");
+        builder.setView(v);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                SharedPreferences mPref = getActivity().getSharedPreferences("info", Context.MODE_PRIVATE);
+                mPref.edit().putInt("id_field",Integer.parseInt(mIDNumberField.getText().toString())).apply();
+                mPref.edit().putInt("pin_field", Integer.parseInt(mPinNumberField.getText().toString())).apply();
+                mPref.edit().putString("username_field", mUsernameField.getText().toString()).apply();
+
+            }
+        });
+        final AlertDialog alert = builder.create();
+        alert.show();
         if (mPref.contains("id_field")
                 && mPref.contains("username_field")
                 && mPref.contains("pin_field")) {
@@ -38,57 +56,110 @@ public class InfoEntryFragment extends DialogFragment {
             mPinNumberField.setText(Integer.toString(mPref.getInt("pin_field", 1234)));
         }
 
-        builder.setTitle("Enter Info")
-            .setView(v)
-            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    attemptRegister();
-                }
-            });
-
-        return builder.create();
-    }
-
-
-    //TODO this is awkward, because it'll leave dialog... don't let that happen
-    public boolean attemptRegister() {
-        mIDNumberField.setError(null);
-        mPinNumberField.setError(null);
         mUsernameField.setError(null);
+        mPinNumberField.setError(null);
+        mIDNumberField.setError(null);
+
+
 
         String mID = mIDNumberField.getText().toString();
-        String mPIN = mPinNumberField.getText().toString();
         String mName = mUsernameField.getText().toString();
+        String mPIN = mPinNumberField.getText().toString();
 
-        boolean cancel = false;
-        View focusView = null;
-
-        if(TextUtils.isEmpty(mID)) {
+        if (TextUtils.isEmpty(mID)) {
             mIDNumberField.setError("ID Number Required");
-            focusView = mIDNumberField;
-            cancel = true;
-        } //TODO make sure int
-
-        if (mPIN.length() != 4) {
-            mPinNumberField.setError("4 Digit PIN required");
-            focusView = mPinNumberField;
-            cancel = true;
-        } //TODO make sure int
-
+            alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+        }
         if (TextUtils.isEmpty(mName)) {
             mUsernameField.setError("Username Required");
-            focusView = mUsernameField;
-            cancel = true;
+            alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+        }
+        if(TextUtils.isEmpty(mPIN)) {
+            mPinNumberField.setError("4 digit PIN Required");
+            alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
         }
 
-        if (cancel) {
-            focusView.requestFocus();
-        } else {
-            SharedPreferences mPref = getActivity().getSharedPreferences("info", Context.MODE_PRIVATE);
-            mPref.edit().putInt("id_field",Integer.parseInt(mIDNumberField.getText().toString())).apply();
-            mPref.edit().putInt("pin_field", Integer.parseInt(mPinNumberField.getText().toString())).apply();
-            mPref.edit().putString("username_field", mUsernameField.getText().toString()).apply();
-        }
-        return !cancel;
+
+
+
+        mIDNumberField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                String mID = charSequence.toString();
+                if (TextUtils.isEmpty(mID)) { //set or reset error
+                    mIDNumberField.setError("ID Number Required");
+                    alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+                } else { //clear error and check if other fields have errors.
+                    mIDNumberField.setError(null);
+                    if(mUsernameField.getError() == null && mPinNumberField.getError() == null)
+                    alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mUsernameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                String mName = mUsernameField.getText().toString();
+                if (TextUtils.isEmpty(mName)) {
+                    mUsernameField.setError("Username Required");
+                    alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+                }
+                else {
+                    mUsernameField.setError(null);
+                    if(mPinNumberField.getError() == null && mIDNumberField.getError() == null)
+                        alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mPinNumberField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                String mPIN = mPinNumberField.getText().toString();
+                    if (mPIN.length() != 4) {
+                        mPinNumberField.setError("4 Digit PIN required");
+                        alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+                    }
+                    else {
+                        mPinNumberField.setError(null);
+                        if(mUsernameField.getError() == null && mIDNumberField.getError() == null)
+                            alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
+                    }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return alert;
+
     }
 }
